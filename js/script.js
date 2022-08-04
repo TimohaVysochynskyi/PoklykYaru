3c32c025de8f13afb0866b556d599cb18fa1f5f5
TimohaVysochynskyi <70945682+TimohaVysochynskyi@users.noreply.github.com>
2022-08-05 00:57:31 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,46 +2,64 @@ window.onload = setTimeout(function(){
     $("#preloader").slideUp("slow");
 }, 2000);
 
-$('.whoarewe').hover( function(){
-    function textAnimate(){
-        document.getElementById('whoarewe-text').innerHTML = "Мета «Поклик Яру» - виховання відповідальної молоді. <br>Ми переконані, що це найкоротший шлях до процвітання і розвитку нашої Держави. <br>Наша команда — це свідома українська молодь, об'єднана цінностями Побратимства, Дисципліни та Патріотизму."
-        document.getElementById('whoarewe-text').style.fontSize = "16px";
-        document.getElementById('whoarewe-text').style.fontWeight = "600";
-        document.getElementById('whoarewe-text').style.lineHeight = "20px";
-        document.getElementById('whoarewe-text').style.textAlign = "left";
-        document.getElementById('whoarewe-text').style.paddingLeft = "15vh";
-        document.getElementById('whoarewe-text').style.paddingRight= "5vh";
-        document.getElementById('whoarewe-text').style.transform = "translateX(-16vw)";
-    }
-    setTimeout(textAnimate, 400);
-    document.getElementById('whoarewe-img').style.transform = "translateX(66vw) scale(1.3)";
-    document.getElementById('whoarewe-img').style.transition = "2s";
-    document.getElementById('whoarewe-img').style.transitionTimingFunction = "ease";
-    if(window.screen.width <= 1000){
-        document.getElementById('whoarewe-text').style.padding = "0px";
-        document.getElementById('whoarewe-text').style.transform = "translateX(0vw)";
-    }
-});
-$('.whatarewedoing').hover( function(){
-    function textAnimate(){
-        document.getElementById('whatarewedoing-text').innerHTML = "Наша модель виховання — це систематична і регулярна робота з молоддю протягом років. <br>Ми організовуємо:<br>Табори національно-патріотичного виховання. <br>Освітні, виховні, вишкільні, культурні, туристичні заходи.<br>Популяризуємо історичний регіон Холодний Яр.<br>Волонтерські заходи на підтримку Збройних Сил України"
-        document.getElementById('whatarewedoing-text').style.fontSize = "16px";
-        document.getElementById('whatarewedoing-text').style.fontWeight = "600";
-        document.getElementById('whatarewedoing-text').style.textAlign = "left";
-        document.getElementById('whatarewedoing-text').style.lineHeight = "20px";
-        document.getElementById('whatarewedoing-text').style.paddingLeft = "10vh";
-        document.getElementById('whatarewedoing-text').style.paddingRight= "5vh";
-        document.getElementById('whatarewedoing-text').style.transform = "translateX(16vw)";
-    }
-    setTimeout(textAnimate, 400);
-    document.getElementById('whatarewedoing-img').style.transform = "translateX(-68vw) scale(1.3)";
-    document.getElementById('whatarewedoing-img').style.transition = "2s";
-    document.getElementById('whatarewedoing-img').style.transitionTimingFunction = "ease";
-    if(window.screen.width <= 1000){
-        document.getElementById('whatarewedoing-text').style.padding = "0px";
-        document.getElementById('whatarewedoing-text').style.transform = "translateX(0vw)";
-    }
-});
+if(window.screen.width > 968){
+    $('.whoarewe').hover( function(){
+        function textAnimate(){
+            document.getElementById('whoarewe-text').innerHTML = "Мета «Поклик Яру» - виховання відповідальної молоді. <br>Ми переконані, що це найкоротший шлях до процвітання і розвитку нашої Держави. <br>Наша команда — це свідома українська молодь, об'єднана цінностями Побратимства, Дисципліни та Патріотизму."
+            document.getElementById('whoarewe-text').style.fontSize = "16px";
+            document.getElementById('whoarewe-text').style.fontWeight = "600";
+            document.getElementById('whoarewe-text').style.lineHeight = "20px";
+            document.getElementById('whoarewe-text').style.textAlign = "left";
+            document.getElementById('whoarewe-text').style.paddingLeft = "15vh";
+            document.getElementById('whoarewe-text').style.paddingRight= "5vh";
+            document.getElementById('whoarewe-text').style.transform = "translateX(-16vw)";
+        }
+        setTimeout(textAnimate, 400);
+        document.getElementById('whoarewe-img').style.transform = "translateX(66vw) scale(1.3)";
+        document.getElementById('whoarewe-img').style.transition = "2s";
+        document.getElementById('whoarewe-img').style.transitionTimingFunction = "ease";
+        if(window.screen.width <= 968){
+            document.getElementById('whoarewe-text').style.padding = "0px";
+            document.getElementById('whoarewe-text').style.transform = "translateX(0vw)";
+        }
+    });
+    $('.whatarewedoing').hover( function(){
+        function textAnimate(){
+            document.getElementById('whatarewedoing-text').innerHTML = "Наша модель виховання — це систематична і регулярна робота з молоддю протягом років. <br>Ми організовуємо:<br>Табори національно-патріотичного виховання. <br>Освітні, виховні, вишкільні, культурні, туристичні заходи.<br>Популяризуємо історичний регіон Холодний Яр.<br>Волонтерські заходи на підтримку Збройних Сил України"
+            document.getElementById('whatarewedoing-text').style.fontSize = "16px";
+            document.getElementById('whatarewedoing-text').style.fontWeight = "600";
+            document.getElementById('whatarewedoing-text').style.lineHeight = "20px";
+            document.getElementById('whatarewedoing-text').style.textAlign = "left";
+            document.getElementById('whatarewedoing-text').style.paddingLeft = "10vh";
+            document.getElementById('whatarewedoing-text').style.paddingRight= "5vh";
+            document.getElementById('whatarewedoing-text').style.transform = "translateX(16vw)";
+        }
+        setTimeout(textAnimate, 400);
+        document.getElementById('whatarewedoing-img').style.transform = "translateX(-68vw) scale(1.3)";
+        document.getElementById('whatarewedoing-img').style.transition = "2s";
+        document.getElementById('whatarewedoing-img').style.transitionTimingFunction = "ease";
+        if(window.screen.width <= 968){
+            document.getElementById('whatarewedoing-text').style.padding = "0px";
+            document.getElementById('whatarewedoing-text').style.transform = "translateX(0vw)";
+        }
+    });
+}
+
+function whoAreWe(){
+    document.getElementById('whoarewe-text').innerHTML = "Мета «Поклик Яру» - виховання відповідальної молоді. <br>Ми переконані, що це найкоротший шлях до процвітання і розвитку нашої Держави. <br>Наша команда — це свідома українська молодь, об'єднана цінностями Побратимства, Дисципліни та Патріотизму."
+    document.getElementById('whoarewe-text').style.fontSize = "16px";
+    document.getElementById('whoarewe-text').style.fontWeight = "600";
+    document.getElementById('whoarewe-text').style.lineHeight = "20px";
+    document.getElementById('whoarewe-text').style.textAlign = "left";
+}
+function whatAreWeDoing(){
+    document.getElementById('whatarewedoing-text').innerHTML = "Наша модель виховання — це систематична і регулярна робота з молоддю протягом років. <br>Ми організовуємо:<br>Табори національно-патріотичного виховання. <br>Освітні, виховні, вишкільні, культурні, туристичні заходи.<br>Популяризуємо історичний регіон Холодний Яр.<br>Волонтерські заходи на підтримку Збройних Сил України"
+    document.getElementById('whatarewedoing-text').style.fontSize = "16px";
+    document.getElementById('whatarewedoing-text').style.fontWeight = "600";
+    document.getElementById('whatarewedoing-text').style.lineHeight = "20px";
+    document.getElementById('whatarewedoing-text').style.textAlign = "left";
+}
+
 function aboutUsHeader(){
     if(window.screen.width > 968){
         if(document.getElementById('aboutusheader2').style.display == "block"){
@@ -91,6 +109,7 @@ function merchHeader(){
         }
     }
 }
+
 function showFeedback(){
     if(document.getElementById('showfeedbackbtn').innerText == "БІЛЬШЕ ВІДГУКІВ"){
         document.getElementById('showfeedbackbtn').innerText = "МЕНШЕ ВІДГУКІВ";
