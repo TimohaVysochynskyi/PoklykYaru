0cbfa8ecd41c412d0a2203e03ec2744bbc0b4d43
TimohaVysochynskyi <70945682+TimohaVysochynskyi@users.noreply.github.com>
2022-08-05 16:51:12 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -79,15 +79,24 @@ function whatAreWeDoing(){
     document.getElementById('whatarewedoing-text').style.lineHeight = "20px";
     document.getElementById('whatarewedoing-text').style.textAlign = "left";
 }
-
+function showBorder(){
+    document.getElementById('aboutusheader').style.borderColor = "#293C43";
+    document.getElementById('aboutusheader').style.borderBottom = "2px solid #293C43";
+}
+function hideBorder(){
+    document.getElementById('aboutusheader').style.borderColor = "rgba(0, 0, 0, 0)";
+}
 function aboutUsHeader(){
     if(window.screen.width > 968){
         if(document.getElementById('aboutusheader2').style.display == "block"){
             document.getElementById('aboutusheader').style.borderRadius = "24.5px";
+            document.getElementById('aboutusheader').style.borderColor = "rgba(0, 0, 0, 0)";
             document.getElementById('aboutusheader2').style.display = "none";
             document.getElementById('aboutusheader3').style.display = "none";
         }else{
             document.getElementById('aboutusheader').style.borderRadius = "20px 20px 0px 0px";
+            document.getElementById('aboutusheader').style.borderBottom = "none";
+            document.getElementById('aboutusheader').style.borderColor = "#293C43";
             document.getElementById('aboutusheader2').style.display = "block";
             document.getElementById('aboutusheader3').style.display = "block";
         }
@@ -96,15 +105,18 @@ function aboutUsHeader(){
 function eventHeader(){
     if(window.screen.width > 968){
         if(document.getElementById('eventheader2').style.display == "block"){
+            document.getElementById('eventheader').style.borderRadius = "24.5px";
+            document.getElementById('eventheader').style.borderColor = "rgba(0, 0, 0, 0)";
             document.getElementById('eventheader2').style.display = "none";
             document.getElementById('eventheader3').style.display = "none";
             document.getElementById('eventheader4').style.display = "none";
             document.getElementById('eventheader5').style.display = "none";
             document.getElementById('eventheader6').style.display = "none";
             document.getElementById('eventheader7').style.display = "none";
-            document.getElementById('eventheader').style.borderRadius = "24.5px";
         } else {
             document.getElementById('eventheader').style.borderRadius = "20px 20px 0px 0px";
+            document.getElementById('eventheader').style.borderBottom = "none";
+            document.getElementById('eventheader').style.borderColor = "#293C43";
             document.getElementById('eventheader2').style.display = "block";
             document.getElementById('eventheader3').style.display = "block";
             document.getElementById('eventheader4').style.display = "block";
@@ -118,11 +130,14 @@ function merchHeader(){
     if(window.screen.width > 968){
         if(document.getElementById('merchheader2').style.display == "block"){
             document.getElementById('merchheader').style.borderRadius = "24.5px";
+            document.getElementById('merchheader').style.borderColor = "rgba(0, 0, 0, 0)";
             document.getElementById('merchheader2').style.display = "none";
             document.getElementById('merchheader3').style.display = "none";
             document.getElementById('merchheader4').style.display = "none";
         } else {
             document.getElementById('merchheader').style.borderRadius = "20px 20px 0px 0px";
+            document.getElementById('merchheader').style.borderBottom = "none";
+            document.getElementById('merchheader').style.borderColor = "#293C43";
             document.getElementById('merchheader2').style.display = "block";
             document.getElementById('merchheader3').style.display = "block";
             document.getElementById('merchheader4').style.display = "block";
@@ -159,3 +174,34 @@ function showFeedback(){
         }
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const deadline = new Date(2023, 06, 01);
+    // id таймера
+    let timerId = null;
+    // склонение числительных
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+      const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      $days.textContent = days < 10 ? '0' + days : days;
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+    }
+    // получаем элементы, содержащие компоненты даты
+    const $days = document.querySelector('#days');
+    const $hours = document.querySelector('#hours');
+    const $minutes = document.querySelector('#minutes');
+    // вызываем функцию countdownTimer
+    countdownTimer();
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+  });
